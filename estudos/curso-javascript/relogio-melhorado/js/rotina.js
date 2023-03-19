7187656492609bc033e032277b35141e27954012
João Pedro <jpedromouramendes@gmail.com>
2023-03-19 19:43:05 -0300

    Site de rotinas aparentemente finalizado

--- estudos/curso-javascript/relogio-melhorado/js/rotina.js
+++ estudos/curso-javascript/relogio-melhorado/js/rotina.js
@@ -50,16 +50,16 @@ function adicionarRotina() {
     const inputInicioNum = document.getElementById('inicionum');
     const inputFimNum = document.getElementById('fimnum');
     const listaRotina = document.getElementById('listarotinas');
-    
-    //Verifica se tem algum erro
 
     //Pega os valores dos inputs
     let nomeRotina = inputNomeRotina.value;
     let inicioNum = inputInicioNum.value[0] + inputInicioNum.value[1] + inputInicioNum.value[3] + inputInicioNum.value[4];
     let fimNum = inputFimNum.value[0] + inputFimNum.value[1] + inputFimNum.value[3] + inputFimNum.value[4];
-    //Converte as horas que estão em string para int
-    inicioNum = parseInt(inicioNum);
-    fimNum = parseInt(fimNum);
+
+    //Verifica se tem algum erro
+    if (verificarErros(inicioNum, fimNum)) {
+        return;
+    }
 
     //Salvar a rotina
     nome.push(nomeRotina);
@@ -78,12 +78,168 @@ function adicionarRotina() {
     inputFimNum.value = '';
 }
 
+function verificarErros(inicioRot, fimRot) {
+    const inputNomeRotina = document.getElementById('nomerotina');
+    const inputInicioNum = document.getElementById('inicionum');
+    const inputFimNum = document.getElementById('fimnum');
+
+    //Verifica se todos os inputs foram preenchidos
+    if (inputNomeRotina.value.length == 0 || inputInicioNum.value.length == 0 || inputFimNum.value.length == 0) {
+        alert('[ERRO] Preencha todos os campos')
+        return true;
+    }
+
+    //Verifica se a rotina não começa e termina no mesmo tempo
+    if (inicioRot == fimRot) {
+        alert('[ERRO] O início da rotina não pode ser ao mesmo tempo que se termina');
+        return true;
+    }
+
+    //Verifica se a rotina interfere no horário de outras
+    //Pegando as horas e minutos separados
+    let inicioHr = inicioRot.substring(0, 2);
+    let inicioMin = inicioRot.substring(2, 4);
+    let fimHr = fimRot.substring(0, 2);
+    let fimMin = fimRot.substring(2, 4);
+
+    //Pegando a duração da rotina
+    let inicio = new Date(`2023-03-18T${inicioHr}:00:00`);
+    let fim = new Date(`2023-03-18T${fimHr}:00:00`);
+    let diferencao = new Date(fim - inicio);
+    let duracaoRotinaHr = diferencao.getUTCHours();
+
+    for (let x = 0; x < nome.length; x++) {
+        //Calcula Horas
+        let minutos = parseInt(inicioMin);
+        for (let y = 0; y <= duracaoRotinaHr; y++) {
+            //Verifica se a hora da rotina atual bate com alguma das outras rotinas
+            let inicial = new Date(`2023-03-18T${inicioHr}:00:00`);
+            inicial.setHours(inicial.getHours() + y);
+            let hora = inicial.getHours();
+            //Pegando as horas das rotinas salvas
+            let inicioHrSalva = inicioHoras[x].substring(0, 2);
+            let fimHrSalva = fimHoras[x].substring(0, 2);
+            //Pegando os minutos das rotinas salvas
+            let inicioMinSalva = inicioHoras[x].substring(2, 4);
+            let fimMinSalva = fimHoras[x].substring(2, 4);
+
+            if (hora == inicioHrSalva || hora == fimHrSalva) {
+                if (y == 0 && hora == fimHrSalva || y == duracaoRotinaHr && hora == inicioHrSalva) {} else {
+                    alert('[ERRO]2');
+                    return true;
+                }
+            }
+
+            //Verifica os minutos
+            if (duracaoRotinaHr == 0) {
+                for (minutos; minutos < fimMin; minutos++) {
+                    if (hora == inicioHrSalva || hora == fimHrSalva) {
+                        if (minutos == inicioMinSalva || minutos == fimMinSalva) {
+                            alert('deu certo aaaaa');
+                            return true;
+                        }
+                    }
+                }
+            } else if (y == duracaoRotinaHr) {
+                for (minutos; minutos < fimMin; minutos++) {
+                    if (hora == inicioHrSalva || hora == fimHrSalva) {
+                        if (minutos == inicioMinSalva || minutos == fimMinSalva) {
+                            alert('deu certo aaaaa');
+                            return true;
+                        }
+                    }
+                }
+            } else {
+                for (minutos; minutos < 60; minutos++) {
+                    if (hora == inicioHrSalva || hora == fimHrSalva) {
+                        if (minutos == inicioMinSalva || minutos == fimMinSalva) {
+                            alert('deu certo aaaaa');
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            if (y == 0) {
+                minutos = parseInt(inicioMinSalva);
+            } else {
+                minutos = 0;
+            }
+
+            //Verifica se a hora das outras rotinas bate com a atual
+
+            //Calcular duração da rotina salva
+            let inicioSalv = new Date(`2023-03-18T${inicioHrSalva}:00:00`);
+            let fimSalv = new Date(`2023-03-18T${fimHrSalva}:00:00`);
+            let diferencaoSalv = new Date(fimSalv - inicioSalv);
+            let duracaoRotinaHrSalv = diferencaoSalv.getUTCHours();
+
+            for (let z = 0; z <= duracaoRotinaHrSalv; z++) {
+                let inicialSalv = new Date(`2023-03-18T${inicioHrSalva}:00:00`);
+                inicialSalv.setHours(inicialSalv.getHours() + z);
+                let horaSalv = inicialSalv.getHours();
+
+                if (horaSalv == inicioHr || horaSalv == fimHr) {
+                    if (z == 0 && horaSalv == fimHr || z == duracaoRotinaHrSalv && horaSalv == inicioHr) {} else {
+                        alert('[ERRO]4');
+                        return true;
+                    }
+                }
+
+                //Verifica os minutos
+                if (duracaoRotinaHrSalv == 0) {
+                    for (minutos; minutos < fimMinSalva; minutos++) {
+                        if (horaSalv == inicioHr || horaSalv == fimHr) {
+                            if (minutos == inicioMin || minutos == fimMin) {
+                                alert('deu certo aaaaa');
+                                return true;
+                            }
+                        }
+                    }
+                } else if (y == duracaoRotinaHrSalv) {
+                    for (minutos; minutos < fimMinSalva; minutos++) {
+                        if (horaSalv == inicioHr || horaSalv == fimHr) {
+                            if (minutos == inicioMin || minutos == fimMin) {
+                                alert('deu certo aaaaa');
+                                return true;
+                            }
+                        }
+                    }
+                } else {
+                    for (minutos; minutos < 60; minutos++) {
+                        if (horaSalv == inicioHr || horaSalv == fimHr) {
+                            if (minutos == inicioMin || minutos == fimMin) {
+                                alert('deu certo aaaaa');
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            minutos = 0;
+        }
+    }
+
+    return false;
+}
+
 function detectarRotinaAtual(horasAtual) {
     let rotinaAtual = '';
     for (let index = 0; index < inicioHoras.length; index++) {
         if (horasAtual >= inicioHoras[index] && horasAtual < fimHoras[index]) {
             rotinaAtual = nome[index];
             return rotinaAtual;
+        }
+        //Caso a rotina comece em um dia e acabe em outro
+        if (inicioHoras[index] > fimHoras[index]) {
+            let fimHorasAjus = fimHoras[index] * 10;
+            let horasAtualAjus = horasAtual * 10;
+
+            if (horasAtualAjus < fimHorasAjus) {
+                rotinaAtual = nome[index];
+                return rotinaAtual;
+            }
         }
     }
 }
